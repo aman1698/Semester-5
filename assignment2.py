fae3363210836df8fb716fbd5f899c2eb55eaa65
Aman Gupta <32537069+aman1698@users.noreply.github.com>
2019-09-06 04:53:58 -0400

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -3,16 +3,18 @@ dict={
 	"1MS17IS002":"Ayush",
 	"1MS17IS003":"Aditya",
 	"1MS17IS004":"Pranjal",
-	"1MS17IS006":"Himanshu"
+	"1MS17IS014":"Himanshu"
       }
-l1=dict.keys()
+list = [] 
+for key in dict.keys(): 
+        list.append(key) 
 #print(l1)
-for i in range (0,len(l1)):
+for i in range (0,len(list)):
 	#print(l1[i])
-	a=l1[i]
+	a=list[i]
+	#print(a)
 	b=int(a[7:10])
-	#print(type(b))
+	#print(b)
 	if(b%2==0):
-		print(l1[i])
+		print(list[i])
 		print(dict.get(a))
-
