a16ea7ba77ad153dbe6196c649877899bc5e5635
aman1698 <gupta.aman1602@gmail.com>
2019-11-12 20:47:15 -0500

    Recently added

--- Python/prg7.py
+++ Python/prg7.py
@@ -1,18 +1,23 @@
 def C_to_F(temp):
 	res=(temp*1.8)+32
-	print(res)
+	return(res)
 	
 def F_to_C(temp):
 	res=(temp-32)/1.8
-	print(res)
+	return(res)
 
+
+li1=[]
 while True:
 	res=int(input("Enter 1 - Celsius to Fahrenheit\n 2- Fahrenheit to Celsius\n"))
 
 	if (res == 1):
 		temp=int(input("Enter the temp in Celsius\n"))
-		C_to_F(temp)
+		c=C_to_F(temp)
+		li1.append(str(temp)+"---->"+str(c))
+		print(li1)
 	if (res == 2):
 		temp=int(input("Enter the temp in Fahrenheit\n"))
-		F_to_C(temp)
-	
+		f=F_to_C(temp)
+		li1.append(str(temp)+"---->"+str(f))
+		print(li1)
